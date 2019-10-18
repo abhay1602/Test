82b4257287e18500fa1b6517aa66039706e025bd
akolapkar <akolapkar@paypal.com>
2019-10-18 11:48:15 +0800

    Add pom and Fish changes in Solution

--- testsingtel/src/main/java/Solution.java
+++ testsingtel/src/main/java/Solution.java
@@ -5,6 +5,25 @@ public class Solution {
         bird.walk();
         bird.fly();
         bird.sing();
+        Duck duck = new Duck();
+        duck.swim();
+        duck.fly();
+        Parrot p = new Parrot("Cat");
+        p.sing();
+        Chicken chicken = new Chicken();
+        chicken.sing();
+        Rooster rooster = new Rooster(chicken);
+        rooster.flyRooster();
+        Fish fish = new Fish();
+        fish.swim();
+        fish.walk();
+        fish.sing();
+        Shark shark = new Shark("large","eat other fish");
+        shark.make();
+        shark.sizeAndColor();
+        Clownfish claw = new Clownfish("small","joke");
+        claw.sizeAndColor();
+        claw.make();
     }
 
 }
